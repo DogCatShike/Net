4fb5aadc7ba7f1d9bce45031b54d1efed09f7b53
DogCat_Shike <dcsk.flzj@gmail.com>
2025-05-23 08:42:35 +0800

    <feature>add: 网络

--- Assets/Src_Runtime/Net/Client/ClientMain.cs
+++ Assets/Src_Runtime/Net/Client/ClientMain.cs
@@ -1,13 +1,41 @@
 using System;
 using GameClient.System_Game;
 using UnityEngine;
+using Telepathy;
 
 namespace GameClient {
     public class ClientMain : MonoBehaviour {
+        Client client;
+        bool isTearDown;
+
         public static GameSystem gameSystem;
 
         public static AssetModule assetModule;
         public static InputModule inputModule;
+
+        void Awake() {
+            int port = 12345;
+            int messageSize = 1024;
+            string ip = "127.0.0.1";
+
+            client = new Client(messageSize);
+            client.Connect(ip, port);
+
+            client.OnConnected += () => {
+                Debug.Log("[Client] Connected");
+            };
+
+            client.OnData += (data) => {
+                Debug.Log("[Client] data: " + data.Count + " bytes");
+            };
+
+            client.OnDisconnected += () => {
+                Debug.Log("[Client] Disconnected");
+            };
+
+            Debug.Log("[Client] Connecting to " + ip + ":" + port);
+            Application.runInBackground = true;
+        }
 
         void Start() {
             gameSystem = new GameSystem();
@@ -31,6 +59,10 @@ namespace GameClient {
         }
 
         void Update() {
+            if (client != null) {
+                client.Tick(10);
+            }
+
             float dt = Time.deltaTime;
 
             inputModule.Process();
@@ -47,7 +79,16 @@ namespace GameClient {
         }
 
         void TearDown() {
+            if (isTearDown) {
+                return;
+            }
+            isTearDown = true;
+
             assetModule.UnLoadAll();
+
+            if (client != null) { 
+                client.Disconnect();
+            }
         }
     }
 }
--- Assets/Src_Runtime/Net/Server/ServerMain.cs
+++ Assets/Src_Runtime/Net/Server/ServerMain.cs
@@ -1,8 +1,59 @@
 using System;
 using UnityEngine;
+using Telepathy;
 
 namespace GameServer {
     public class ServerMain : MonoBehaviour {
-        
+        Server server;
+        bool isTearDown;
+
+        void Awake() {
+            int port = 12345;
+            int messageSize = 1024;
+
+            server = new Server(messageSize);
+            server.Start(port);
+            Debug.Log("[Server] Listening on port " + port);
+
+            server.OnConnected += (connId, str) => { // 有客户端连接
+                Debug.Log("[Server] Connected " + connId);
+                server.Send(connId, new byte[] { 1, 2, 3 });
+            };
+
+            server.OnData += (connId, data) => { // 有数据到达
+
+            };
+
+            server.OnDisconnected += (connId) => { // 客户端断开连接
+
+            };
+
+            Application.runInBackground = true;
+        }
+
+        void Update() {
+            if (server != null) {
+                server.Tick(10);
+            }
+        }
+
+        void OnDestroy() {
+
+        }
+
+        void OnApplicationQuit() {
+
+        }
+
+        void TearDown() {
+            if (isTearDown) {
+                return;
+            }
+            isTearDown = true;
+
+            if (server != null) {
+                server.Stop();
+            }
+        }
     }
 }
